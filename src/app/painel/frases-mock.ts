87d1d80d9f2781921973d7acb37953d514bceea9
Juliano <juliano@nationphone.com>
2017-11-05 22:29:21 -0200

    UPDATE - App aprendendo Ingles

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Array<Frase>=[
+  {fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender'},
+  {fraseEng: 'She watch TV', frasePtBr: 'Ela assiste TV'},
+  {fraseEng: 'How are you?', frasePtBr: 'Como vai você?'},
+  {fraseEng: 'He eat bread', frasePtBr: 'Ele come pão'}
+]
